a8a1df457f79de93592a02bf739c9c59963e72f9
Cole Callaway <nccallaway54@gmail.com>
2022-09-15 21:23:49 -0600

    register and login funcs

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 """Movie Ratings."""
 
+from atexit import register
+from urllib import request
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User
 
 
 app = Flask(__name__)
@@ -24,10 +26,56 @@ def index():
     """Homepage."""
     return render_template('homepage.html')
 
+@app.route('/users')
+def user_list():
+    users = User.query.all()
+    return render_template('user_list.html', users=users)
 
-@app.route('/reqister', methods=['POST'])
-def reqister_form():
-    pass
+
+@app.route('/register', methods=['POST'])
+def register_form():
+    return render_template('register_form.html')
+
+@app.route('/register', methods = ['POST'])
+def register_process():
+    
+    email = register_form['email']
+    password = register_form['password']
+    age = int(request.form['age'])
+    zipcode = request.form['zipcode']
+    
+    new_user = User(email=email, password=password, age=age, zipcode=zipcode)
+    
+    db.session.add(new_user)
+    db.session.commit()
+    
+    flash(f'user {email} added.')
+    return redirect('/')
+
+@app.route('/login', methods = ['GET'])
+def login_form():
+    return render_template('login_form.html')
+
+@app.route('/login', methods = ['POST'])
+def login_process():
+    
+    email = request.form['email']
+    password = request.form['password']
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if not user:
+        flash('No such user')
+        
+    if user.password != password:
+        flash('Incorrect password')
+        return redirect('/login')
+    
+    session['user_id'] = user.user_id
+    
+    flash('Logged in')
+    return redirect(f'/user/{user.user_id}')
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
